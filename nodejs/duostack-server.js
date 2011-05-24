32692ff5c8c79122dffba83e3a412c43c363906f
minghua <minghuascode@yahoo.ca>
2011-05-24 16:39:21 -0700

    add loop to server.js

--- nodejs/duostack-server.js
+++ nodejs/duostack-server.js
@@ -2,6 +2,7 @@
 var http = require('http');
 
 var cnt=0;
+var loopcnt=0;
 var pathshown=false;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -19,7 +20,9 @@ http.createServer(function (req, res) {
                query: 'name=ryan',
                pathname: '/status' }
            */
-  if ( q.pathname.indexOf('/showadmin') == 0 ) {
+  if ( q.pathname.indexOf('/startloop') == 0 ) {
+    setTimeout(loopfunc,1000);
+  } else if ( q.pathname.indexOf('/showadmin') == 0 ) {
     ostr += "  __dirname:  " + __dirname + "\n";
     ostr += "  __filename: " + __filename + "\n";
     ostr += "  req method:  " + req.method + "\n";
@@ -33,9 +36,19 @@ http.createServer(function (req, res) {
   } else {
     cnt++;
   }
-  ostr += "times this page has been viewed:  " + cnt + "\n";
+  ostr += "Times this page has been viewed:  " + cnt + "\n";
+  var h=parseInt( loopcnt/3600);
+  var m=parseInt( (loopcnt % 3600) / 60);
+  var s=parseInt( (loopcnt % 60) );
+  ostr += "Loops this page has run:          " + loopcnt + "\n";
+  ostr += "                         " + h + " " + m + " " + s + "\n";
   res.end(ostr);
 }).listen(8124, "127.0.0.1");
+
+function loopfunc() {
+  loopcnt++;
+  setTimeout(loopfunc,1000);
+};
 
 console.log('Server running at http://127.0.0.1:8124/');
 
